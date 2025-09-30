140efef86845a36e8f5245dd89802bc6c60d933a
Micael <poinenmicael@gmail.com>
2025-09-30 14:37:46 +0200

    TP04

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -2,42 +2,55 @@ section .data
 
 section .bss
 
-buf resb 32
+    buf resb 32  
 
 section .text
 
 global _start
+
 _start:
 
-    xor eax,eax
-    xor edi,edi
-    lea rsi,[rel buf]
-    mov edx,32
+    mov eax, 0
+    mov edi, 0 
+    lea rsi, [rel buf]
+    mov edx, 32
     syscall
-    
-    xor ecx,ecx
-    mov rbx,buf
 
-.next:
-    mov al,[rbx]
-    cmp al,10
+    xor rcx, rcx 
+    lea rbx, [rel buf]
+
+.parse:
+    mov al, [rbx]
+    cmp al, 0x0A
     je .done
-    sub al,'0'
-    imul ecx,ecx,10
-    add ecx,eax
+    cmp al, 0
+    je .done
+    cmp al, '0'
+    jb .badinput
+    cmp al, '9'
+    ja .badinput
+    sub al, '0'
+    imul rcx, rcx, 10
+    add rcx, rax
     inc rbx
-    jmp .next
+    jmp .parse
 
 .done:
-    test ecx,1
-    jnz .odd
+    test rcx, 1
+    jz .even
+    jmp .odd
 
 .even:
-    mov eax,60
-    xor edi,edi
+    mov eax, 60 
+    xor edi, edi
     syscall
 
 .odd:
-    mov eax,60
-    mov edi,1
+    mov eax, 60
+    mov edi, 1 
+    syscall
+
+.badinput:
+    mov eax, 60
+    mov edi, 2
     syscall
