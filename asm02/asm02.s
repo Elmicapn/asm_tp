6977e5d4280f72e49d1725b7c0f61279ce5d73e5
Micael <poinenmicael@gmail.com>
2025-09-30 11:54:11 +0200

    testTP02

--- asm02/asm02.s
+++ asm02/asm02.s
@@ -1,18 +1,41 @@
 section .data
 
-msg db "1337", 10
-len  equ $ - msg
+msg db "1337",10
+len equ $-msg
+
+section .bss
+buf resb 3 
 
 section .text
 global _start
-
 _start:
-    mov rax, 1 
-    mov rdi, 1
-    mov rsi, msg
-    mov rdx, len     
+    xor eax,eax 
+    xor edi,edi
+    lea rsi,[rel buf]
+    mov edx,3
     syscall
 
-    mov rax, 60 
-    xor rdi,0 
+    cmp eax,2 
+    jb  fail
+    cmp byte [buf],   '4'
+    jne fail
+    cmp byte [buf+1], '2'
+    jne fail
+    cmp eax,2
+    je  ok
+    cmp byte [buf+2], 10
+    jne fail
+
+ok: mov eax,1
+    mov edi,1
+    lea rsi,[rel msg]
+    mov edx,len
+    syscall
+    mov eax,60
+    xor edi,edi
+    syscall
+
+fail:
+    mov eax,60
+    mov edi,1
     syscall
