0132d839d7bcc820b9c88891c69e370b707a07e2
Micael <poinenmicael@gmail.com>
2025-10-02 18:33:41 +0200

    TP09

---
+++ asm09/asm09.s
@@ -0,0 +1,107 @@
+section .bss
+    outbuf resb 128
+
+section .data
+    digits db "0123456789ABCDEF"
+
+section .text
+    global _start
+
+_start:
+    mov     rbx, rsp
+    mov     rdi, [rbx]
+    cmp     rdi, 2
+    jl      no_param
+
+    mov     rsi, [rbx+16]
+    mov     al, [rsi]
+    cmp     al, '-'
+    jne     .default_hex
+    cmp     byte [rsi+1], 'b'
+    jne     .default_hex
+    cmp     byte [rsi+2], 0
+    jne     .default_hex
+    cmp     rdi, 3
+    jl      no_param
+    mov     rsi, [rbx+24]
+    mov     r9, 2
+    jmp     .parse
+
+.default_hex:
+    mov     r9, 16
+
+.parse:
+    call    atoi
+    cmp     rax, -1
+    je      badinput
+    call    itoa_base
+
+    mov     eax, 1
+    mov     edi, 1
+    mov     rsi, rbx
+    mov     rdx, rcx
+    syscall
+
+    mov     eax, 60
+    xor     edi, edi
+    syscall
+
+no_param:
+    mov     eax, 60
+    mov     edi, 1
+    syscall
+
+badinput:
+    mov     eax, 60
+    mov     edi, 2
+    syscall
+
+atoi:
+    xor     rax, rax
+    
+.next:
+    mov     bl, [rsi]
+    cmp     bl, 0
+    je      .done
+    cmp     bl, '0'
+    jb      .error
+    cmp     bl, '9'
+    ja      .error
+    sub     bl, '0'
+    imul    rax, rax, 10
+    movzx   r8, bl
+    add     rax, r8
+    inc     rsi
+    jmp     .next
+
+.done:
+    ret
+
+.error:
+    mov     rax, -1
+    ret
+
+itoa_base:
+    lea     rsi, [outbuf+127]
+    mov     byte [rsi], 10
+    mov     rcx, 1
+    test    rax, rax
+    jnz     .loop
+    dec     rsi
+    mov     byte [rsi], '0'
+    inc     rcx
+    jmp     .finish
+
+.loop:
+    xor     rdx, rdx
+    div     r9
+    dec     rsi
+    mov     bl, [digits+rdx]
+    mov     [rsi], bl
+    inc     rcx
+    test    rax, rax
+    jnz     .loop
+
+.finish:
+    mov     rbx, rsi
+    ret
