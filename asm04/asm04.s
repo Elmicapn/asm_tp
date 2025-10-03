9cdbec15cb6b2645055f726ff1cddff7ca8eaec6
Micael <poinenmicael@gmail.com>
2025-10-03 19:18:15 +0200

    correction2

--- asm04/asm04.s
+++ asm04/asm04.s
@@ -1,82 +1,74 @@
 section .bss
-    buf resb 64
+buf     resb    64
 
 section .text
-    global _start
+global _start
 
 _start:
-    mov eax, 0
-    mov edi, 0
-    lea rsi, [rel buf]
-    mov edx, 64
+    ; read(0, buf, 64)
+    mov     eax, 0
+    mov     edi, 0
+    lea     rsi, [rel buf]
+    mov     edx, 64
     syscall
 
-    xor rcx, rcx
+    xor     rcx, rcx
+    lea     rbx, [rel buf]
+    mov     r8, 0
+    mov     r9, 0
 
-    lea rbx, [rel buf]
-    mov r8, 0
-    mov r9, 0
-
-    mov al, [rbx]
-    cmp al, '-'
-    jne .check_plus
-    
-    mov r9, 1
-    inc rbx
-    jmp .parse
+    mov     al, [rbx]
+    cmp     al, '-'
+    jne     .check_plus
+    mov     r9, 1
+    inc     rbx
+    jmp     .parse
 
 .check_plus:
-    cmp al, '+'
-    jne .parse
-    inc rbx
+    cmp     al, '+'
+    jne     .parse
+    inc     rbx
 
 .parse:
-    mov al, [rbx]
-    cmp al, 0x0A
-    je .done
-    cmp al, 0
-    je .done
-    cmp al, '0'
-    jb .badinput
-    cmp al, '9'
-    ja .badinput
-    sub al, '0'
-
-    mov rax, rcx
-    mov rdx, rcx
-    imul rcx, rcx, 10
-    jo .badinput
-    add rcx, rax
-    jo .badinput
-
-    inc rbx
-    inc r8
-    cmp r8, 19
-    ja .badinput
-    jmp .parse
+    mov     al, [rbx]
+    cmp     al, 0x0A
+    je      .done
+    cmp     al, 0
+    je      .done
+    cmp     al, '0'
+    jb      .badinput
+    cmp     al, '9'
+    ja      .badinput
+    sub     al, '0'
+    imul    rcx, rcx, 10
+    add     rcx, rax
+    inc     rbx
+    inc     r8
+    cmp     r8, 19
+    ja      .badinput
+    jmp     .parse
 
 .done:
-    cmp r9, 0
-    je .check
-    neg rcx
-    jo .badinput
+    cmp     r9, 0
+    je      .check
+    neg     rcx
 
 .check:
-    test rcx, 1
-    jz .even
-    jmp .odd
+    test    rcx, 1
+    jz      .even
+    jmp     .odd
 
 .even:
-    mov eax, 60
-    xor edi, edi
+    mov     eax, 60
+    xor     edi, edi
     syscall
 
 .odd:
-    mov eax, 60
-    mov edi, 1
+    mov     eax, 60
+    mov     edi, 1
     syscall
 
 .badinput:
-    mov eax, 60
-    mov edi, 2
+    mov     eax, 60
+    mov     edi, 2
     syscall
