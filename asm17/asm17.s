a9cec13878f836f4bc90ba773520c9d307ad8f51
Micael <poinenmicael@gmail.com>
2025-10-04 13:41:06 +0200

    correction TP17

--- asm17/asm17.s
+++ asm17/asm17.s
@@ -15,17 +15,22 @@ _start:
     mov     r8, rax
 
     mov     rax, r8
-    xor     rdx, rdx
+    cqo
     mov     rbx, 26
-    div     rbx
+    idiv    rbx 
     mov     r8, rdx
+    cmp     r8, 0
+    jge     .shift_ok
+    add     r8, 26
+.shift_ok:
 
     mov     eax, 0
     mov     edi, 0
     mov     rsi, buffer
     mov     edx, 1024
     syscall
-    mov     r10, rax         ; sauver nb d'octets lus
+
+    mov     r10, rax
     mov     rcx, rax
 
     mov     rsi, buffer
@@ -35,10 +40,12 @@ _start:
     test    rcx, rcx
     jz      .write
     mov     al, [rsi]
+
+    ;minuscules
     cmp     al, 'a'
-    jb      .skip
+    jb      .check_upper
     cmp     al, 'z'
-    ja      .skip
+    ja      .check_upper
     sub     al, 'a'
     add     al, r8b
     movzx   eax, al
@@ -46,8 +53,26 @@ _start:
     div     ebx
     mov     al, dl
     add     al, 'a'
+    jmp     .store
+
+    ;maj
+.check_upper:
+    cmp     al, 'A'
+    jb      .skip
+    cmp     al, 'Z'
+    ja      .skip
+    sub     al, 'A'
+    add     al, r8b
+    movzx   eax, al
+    xor     edx, edx
+    div     ebx
+    mov     al, dl
+    add     al, 'A'
+    jmp     .store
 
 .skip:
+
+.store:
     mov     [rsi], al
     inc     rsi
     dec     rcx
@@ -57,7 +82,7 @@ _start:
     mov     eax, 1
     mov     edi, 1
     mov     rsi, buffer
-    mov     rdx, r10        ; utiliser nb d’octets lus
+    mov     rdx, r10
     syscall
 
     mov     eax, 60
@@ -71,6 +96,12 @@ _start:
 
 atoi:
     xor     rax, rax
+    xor     r9, r9
+    mov     bl, [rsi]
+    cmp     bl, '-'
+    jne     .a_next
+    mov     r9b, 1
+    inc     rsi
 
 .a_next:
     mov     bl, [rsi]
@@ -85,4 +116,9 @@ atoi:
     jmp     .a_next
 
 .a_done:
+    test    r9b, r9b
+    jz      .ret
+    neg     rax
+
+.ret:
     ret
